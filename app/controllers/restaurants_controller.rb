6f1551ed521d47c94a30224243acce2fc6f1a15f
Francesca Cellan-Jones <f.cellanjones@gmail.com>
2020-11-11 20:29:53 +0000

    saving untracked changes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -19,6 +19,6 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :category)
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
