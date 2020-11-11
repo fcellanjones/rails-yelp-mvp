8609981b8e605bd5fcdb4213ae93db08b468f28f
Francesca Cellan-Jones <f.cellanjones@gmail.com>
2020-11-11 16:55:40 +0000

    finished index and create routesfor restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+ping_pong =  { name: "Ping Pong", address: "Southbank Centre, London, SE1 8XX", category: "chinese"}
+le_petit_chat =  { name: "Le Petit Chat", address: "1 High Holborn, London, W1 5TE ", category: "french"}
+doumo =  { name: "Doumo", address: "73 Tower Bridge Rd, London, SE1 4TW", category: "italian"}
+
+[ dishoom, pizza_east, ping_pong, le_petit_chat, doumo ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
